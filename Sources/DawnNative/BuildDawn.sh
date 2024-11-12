2a87ec3b373787fa293dc04f437f2edc1a82259d
Graham Reeves <graham@grahamreeves.com>
2024-11-12 13:16:25 +0000

    Adding instructions for building ios

--- Sources/DawnNative/BuildDawn.sh
+++ Sources/DawnNative/BuildDawn.sh
@@ -5,7 +5,7 @@ cd dawn
 mkdir -p out/Release
 cd out/Release
 #cmake -DBUILD_SHARED_LIBS=0 ../..
-cmake -DBUILD_SHARED_LIBS=1 -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"  ../..
+cmake -DBUILD_SHARED_LIBS=1 -DCMAKE_SYSTEM_NAME=Darwin -DCMAKE_OSX_ARCHITECTURES="x86_64;arm64"  ../..
 make -j 10 webgpu_dawn
 #make dawn_headers # what is this target?
 
@@ -13,3 +13,5 @@ make -j 10 webgpu_dawn
 ls ./src/dawn/native/libwebgpu_dawn.dylib
 ls -r ./gen
 
+# ios
+cmake -DBUILD_SHARED_LIBS=1 -DCMAKE_SYSTEM_NAME=iOS ../..
